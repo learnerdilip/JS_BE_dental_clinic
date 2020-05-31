0884da12837266002e53a84409e74dc750655da8
learnerdilip <diliptalks@gmail.com>
2020-05-31 11:07:15 +0200

    changed inventory endpoint

--- src/inventory/model.js
+++ src/inventory/model.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
 const inventorySchema = new mongoose.Schema({
-  itemName: { type: String, required: true },
-  // itemQuantity: { type: String },
-  // purchaseDate: { type: Date },
-  // expiryDate: { type: Date },
-  dealersName: { type: String },
+  itemName: { type: String, required: true, allowNull: false },
+  itemSpec: { type: String },
+  category: { type: String, required: true, allowNull: false },
+  dealer: { type: String },
+  useQyt: { type: String },
+  stockQyt: { type: String },
+  inUseExpiry: { type: Date },
+  stockExpiry: { type: Date },
 });
 
 module.exports = mongoose.model("inventory", inventorySchema);
