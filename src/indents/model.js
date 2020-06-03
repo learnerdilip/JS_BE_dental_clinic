1d94c464bdeac9d3f99086b627c3d8e41b2be8aa
learnerdilip <diliptalks@gmail.com>
2020-06-03 03:33:25 +0200

    changes to indent model

--- src/indents/model.js
+++ src/indents/model.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 const OrderItem = new mongoose.Schema({
   item: { type: String },
   quantity: { type: String },
-  status: { type: String },
-  price: { type: Number },
+  status: { type: Boolean, default: false },
+  price: { type: Number, required: true },
 });
 
 const IndentSchema = new mongoose.Schema({
   orderDate: { type: Date },
   dealer: { type: String },
   estimatedAmount: { type: Number },
-  paid: { type: Number },
-  balance: { type: Number },
+  due: { type: Number, default: 0 },
+  paid: { type: Number, default: 0 },
+  balance: { type: Number, default: 0 },
   paymentMode: { type: String },
   orderItems: { type: [OrderItem] },
 });
--- src/indents/router.js
+++ src/indents/router.js
@@ -4,7 +4,9 @@ const Indent = require("./model");
 
 router.post("/indent", async (req, res, next) => {
   try {
+    console.log("indents request:", req.body);
     const saveIndent = await Indent.create(req.body);
+    // console.log(saveIndent);
     res.send(saveIndent);
   } catch {
     (error) => console.error(error);
